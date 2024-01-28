2e56bb9c3ee856a7b612a75d5e96de07015e6676
diegoProFP <diego.di1@educa.madrid.org>
2024-01-28 11:35:27 +0100

    Initial commit

---
+++ src/main/java/netflix/Constantes.java
@@ -0,0 +1,8 @@
+package netflix;
+
+public class Constantes {
+    public static final double PRECIO_NETFLIX_TOTAL = 250.0;
+    public static final double DESCUENTO_PAGO_ANUAL = 0.15;   //15% de descuento
+    public static final int PAGO_MENSUAL = 0;
+    public static final int PAGO_ANUAL = 1;
+}
