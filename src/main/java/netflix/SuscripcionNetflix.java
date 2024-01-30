f68d0f3e4834cb3d66dbdceff083ac7d975590ae
Alumno <alumno@max11>
2024-01-30 16:15:51 +0100

    Sergio Simon Fernandez Entornos

--- src/main/java/netflix/SuscripcionNetflix.java
+++ src/main/java/netflix/SuscripcionNetflix.java
@@ -6,12 +6,28 @@ public class SuscripcionNetflix {
     String numTarjeta;
     int numPerfiles;
 
+    /*
+     * Clase que contiene los métodos necesarios para cacular cuanto debe pagar cada usuario segun el plan contratado
+     * Máximo se admitiran 3 usuarios de manera predeterminda
+     * @param email Email del usuario
+     * @param numTarjeta Tarjeta del usuario
+     * @author Sergio Simon Fernández
+     */
+
     public SuscripcionNetflix(String email, String numTarjeta) {
         super();
         this.email = email;
         this.numTarjeta = numTarjeta;
         this.numPerfiles = 3; //por defecto te ofrece 3 perfiles
     }
+
+    /*
+     * Método que calcula el precio de la subscripción dependiendo del modo de pago elegido. Si es anual se le calculará
+     * un descuento, si no lo es se calculara el precio mensual
+     * @param modoPago tipo de subscripcion elegida (Anual o Mensual)
+     * @returns precio de la subscripcion
+     * @author Sergio Simon Fernández
+     */
 
     public double cuotaPagar(int modoPago) {
         double cuota = 0;
@@ -28,16 +44,34 @@ public class SuscripcionNetflix {
         return redondearDoubleDosDecimales(cuota);
     }
 
+    /*
+     * Método que calcula el precio a pagar por cada uno de los usuarios
+     * @param modoPago tipo de subscripcion elegida (Anual o Mensual)
+     * @returns Precio a pagar por cada uno de los perfiles
+     * @author Sergio Simon Fernández
+     */
     public double cuotaPorPerfil(int modoPago) {
         double cuotaPagarTotal = cuotaPagar(modoPago);
         return redondearDoubleDosDecimales(cuotaPagarTotal / this.numPerfiles);
     }
 
+    /*
+     * Método que calcula el precio a pagar por cada uno de los usuarios actualmente en desuso
+     * @param modoPago tipo de subscripcion elegida (Anual o Mensual)
+     * @returns Precio a pagar por cada uno de los perfiles
+     * @author Sergio Simon Fernández
+     */
     @Deprecated
     public double cuotaPerfiles(int modoPago) {
         return redondearDoubleDosDecimales(Constantes.PRECIO_NETFLIX_TOTAL / this.numPerfiles);
     }
 
+    /*
+     * Método que redondea el precio de la subcripcion a dos decimales
+     * @param numero Precio que va a ser redondeado
+     * @returns Precio redondeado
+     * @author Sergio Simon Fernández
+     */
     public static double redondearDoubleDosDecimales(double numero) {
         return (double) Math.round(numero * 100) / 100;
     }
